5e89c933d977c0c07fd2590db85be9f6e88b1bd3
Lucas Wung <tkoppopwung130@gmail.com>
2021-06-14 14:57:09 -0700

    Update CalenderTask.java

--- src/main/model/CalenderTask.java
+++ src/main/model/CalenderTask.java
@@ -1,15 +1,11 @@
 package model;
-
 //represents each task within Calender
 public class CalenderTask {
-
     public int day;
     public String month;
     public int year;
     public String name;
     public int urgency;
-
-
     public CalenderTask(int day, String month, int year, String name, int urgency) {
         this.day = day;
         this.month = month;
@@ -17,33 +13,25 @@ public class CalenderTask {
         this.name = name;
         this.urgency = urgency;
     }
-
     public int getDay() {
         return day;
     }
-
     //EFFECTS: returns the month of the task
     public String getMonth() {
         return month;
     }
-
     //EFFECTS: returns the year of the task
     public int getYear() {
         return year;
     }
-
-
-
     //EFFECTS: returns the name of the task
     public String getName() {
         return name;
     }
-
     //EFFECTS: returns the urgency of the task
     public int getUrgency() {
         return urgency;
     }
-
     //EFFECTS: returns urgent if urgency is 1, and normal urgency if urgency is 0
     public String toStringUrgency() {
         if (getUrgency() == 1) {
@@ -52,6 +40,4 @@ public class CalenderTask {
             return "Not Urgent";
         }
     }
-
-
 }
