269bf1525f5f423129e0b1e24baf84b019eb2583
Lucas Wung <tkoppopwung130@gmail.com>
2021-06-14 15:03:25 -0700

    Update WriterTest.java

--- src/test/persistence/WriterTest.java
+++ src/test/persistence/WriterTest.java
@@ -1,27 +1,20 @@
 package persistence;
-
-
 import model.Calender;
 import model.CalenderTask;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 public class WriterTest {
     public CalenderTask ct = new CalenderTask(4, "Feb", 2020, "English", 1);
     public Calender cal;
-
-
     @BeforeEach
     void runBefore() {
         cal = new Calender();
     }
-
     @Test
     void testWrite() {
         try {
@@ -35,10 +28,8 @@ public class WriterTest {
             assertEquals(2020, ct.getYear());
             assertEquals("English", ct.getName());
             assertEquals(1, ct.getUrgency());
-
         } catch (IOException e) {
             fail("IOException should not have been thrown");
         }
     }
-
 }
