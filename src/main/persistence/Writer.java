bf3703f03b5eb8c794f7b22c1e4ad383ba68b292
Lucas Wung <tkoppopwung130@gmail.com>
2021-06-14 14:58:33 -0700

    Update Writer.java

--- src/main/persistence/Writer.java
+++ src/main/persistence/Writer.java
@@ -1,24 +1,19 @@
 package persistence;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
-
 public class writer {
     private static PrintWriter printWriter;
-
     // EFFECTS: constructs a writer that will write data to file
     public Writer(File file) throws FileNotFoundException, UnsupportedEncodingException {
         printWriter = new PrintWriter(file, "UTF-8");
     }
-
     // MODIFIES: this
     // EFFECTS: writes saveable to file
     public static void write(Saveable saveable) {
         saveable.save(printWriter);
     }
-
     // MODIFIES: this
     // EFFECTS: close print writer
     public static void close() {
